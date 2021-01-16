228293fec3440be9d3c33daed95d26d0a1c45cc6
Martin <martinkabe@seznam.cz>
2021-01-16 11:08:34 +0100

    Clean up CRUD repository

--- webapp/src/test/java/com/springboot/webapp/test/RetrieveDataTest.java
+++ webapp/src/test/java/com/springboot/webapp/test/RetrieveDataTest.java
@@ -7,14 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 @SpringBootTest
 @ActiveProfiles({ "uat", "uat-test" })
-@ComponentScan("com.springboot.hcrud")
 public class RetrieveDataTest {
 
     private final Logger log = LoggerFactory.getLogger(RetrieveDataTest.class);
